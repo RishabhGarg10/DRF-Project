6ba47386d2121dbcd63efcf13f2a38102888e0c9
Rishabh Garg <rishabgar29g@gmail.com>
2026-03-14 19:50:35 +0530

    Serializers : converting querysets,dict etc into JSON and vice versa for GET,POST etc.

--- api/views.py
+++ api/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+#from django.shortcuts import render
+#from django.http import JsonResponse
+from students.models import Student
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import StudentSerializers
 
 # Create your views here.
-def students(request):
-    students = {
-        "name" : "John Doe",
-        "age" : 25,
-        "class" : "Computer Science"
-
-    }
-    return JsonResponse(students)    
+@api_view(['GET','POST'])
+def studentsView(request):
+    if request.method == "GET":
+        students = Student.objects.all()
+        serializer = StudentSerializers(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = StudentSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
